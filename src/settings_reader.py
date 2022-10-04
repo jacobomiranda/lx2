34b68b6a2fde24fe643c9572c02235bb0768b339
mirandaa <mirandaa@mpi-cbg.de>
2022-10-04 10:50:27 +0200

    init: settings reader and settings test

--- src/settings_reader.py
+++ src/settings_reader.py
@@ -2,6 +2,29 @@
 """
 import configparser
 
-def read_settings(ini_path:str):
+
+def read_settings(ini_path: str, section: str):
     config = configparser.ConfigParser()
-    return config.read(ini_path)
+    config.read(ini_path)
+    options = dict(config._sections[section])
+
+    # parse values
+    for k, v in options.items():
+        try:
+            options[k] = eval(v)
+        except SyntaxError as se:
+            pass
+        except NameError as ne:
+            pass
+
+    # assert valid data types
+    ok_types = [int, str, float, tuple]
+    for v in options.values():
+        is_ok = False
+        for t in ok_types:
+            if isinstance(v, t):
+                is_ok = True
+                continue
+        assert is_ok
+
+    return options
--- tests/test_settings_reader.py
+++ tests/test_settings_reader.py
@@ -1,6 +1,9 @@
 import pytest
 from src.settings_reader import read_settings
 
+
 def test_read_settings():
-    options = read_settings(r'..\test_resources\Import_Settings_sample.ini')
-    assert options is not None
+    options = read_settings(
+        r"tests\test_resources\Import_Settings_sample.ini", "General_Import_filtered"
+    )
+    assert options["precursormassshift"] == 0
