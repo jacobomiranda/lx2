792c692a9669c5c7f1f2479ca2114bfbac699e97
mirandaa <mirandaa@mpi-cbg.de>
2022-10-04 11:13:20 +0200

    test: read project

--- src/settings_reader.py
+++ src/settings_reader.py
@@ -18,11 +18,11 @@ def read_settings(ini_path: str, section: str):
             pass
 
     # assert valid data types
-    ok_types = [int, str, float, tuple]
+    ok_types = [int, str, float, tuple]  # none checked below
     for v in options.values():
         is_ok = False
         for t in ok_types:
-            if isinstance(v, t):
+            if isinstance(v, t) or v is None:
                 is_ok = True
                 continue
         assert is_ok
